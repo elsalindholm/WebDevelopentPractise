1739c811aec5d8ce156a9a636af2ddf68989566a
elsalindholm <31487907+elsalindholm@users.noreply.github.com>
2020-10-08 15:01:14 +0100

    Exercise 11 completed

---
+++ Exercise 11/exercise11.js
@@ -0,0 +1,15 @@
+function changeToApples (){
+  document.getElementById("paragraph").innerHTML = "An apple a day keeps the doctor away";
+  document.getElementById("image").src = "./images/appleTree.jpg";
+}
+
+
+function changeToBananas () {
+  document.getElementById("paragraph").innerHTML = "So I'm going bananas, are you going nuts?";
+  document.getElementById("image").src = "./images/bananaTree.jpg";
+}
+
+function changeToGrapes () {
+  document.getElementById("paragraph").innerHTML = "I heard it through the grapevine.";
+  document.getElementById("image").src = "./images/grapeVine.jpg";
+}
