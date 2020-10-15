492c23c3961d9ea221982be166a3765bf99ef919
elsalindholm <31487907+elsalindholm@users.noreply.github.com>
2020-10-15 19:30:08 +0100

    Exercise 14 finished

---
+++ Exercise 14/exercise14.js
@@ -0,0 +1,21 @@
+var num = 346;
+var num2 = 10;
+
+console.log("hello world! ", num);
+
+function Button2(){
+  console.log(num + " times please");
+}
+
+function Button3(){
+  console.log(`You get ${num2} tickles per petting`);
+}
+
+function Button1(){
+  var fatTits = "Pet my titties!";
+  printMessage(fatTits);
+}
+
+function printMessage(msg){
+  console.log(msg);
+}
